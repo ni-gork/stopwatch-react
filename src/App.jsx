c7bce882c4abdbad584cb4636c6842baa4dcfc46
Nikita Gorkov <nigork@taltech.ee>
2025-03-12 23:30:41 +0200

    stopwatch styling

--- src/App.jsx
+++ src/App.jsx
@@ -18,24 +18,25 @@ function App() {
   }, [running]) 
 
   return (
-    <>
-    <h1>STOPWATCH</h1>
-    <div>
-      <span>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)}:</span>
-      <span>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)}:</span>
-      <span>{("0" + (Math.floor(time / 10) % 100)).slice(-2)}</span>
-    </div>
-    <div>
-      {
-        running ? (
-          <button onClick={() => setRunning(false)}>Stop</button>
-        ) : (
-          <button onClick={() => setRunning(true)}>Start</button>
-        )
-      }
-      <button onClick={() => setTime(0)}>Reset</button>
-    </div>
-    </>
+    <div className='container'>
+      <h1>STOPWATCH</h1>
+      <div className='inputs'>
+        <span>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + (Math.floor(time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div className='btns'>
+        {
+          running ? (
+            <button onClick={() => setRunning(false)}>Stop</button>
+          ) : (
+            <button onClick={() => setRunning(true)}>Start</button>
+          )
+        }
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
+    </div>  
+    
   )
 }
 
